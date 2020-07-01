83da5924a01d12190916bf6cde9bd79e07576e2e
Architect <greatraksin@icloud.com>
2020-07-01 11:36:50 +0300

    Adding templates

--- mysite/routes.py
+++ mysite/routes.py
@@ -1,7 +1,18 @@
+from flask import render_template
 from mysite import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h1>Добро пожаловать на мой сайт!</h1>'
+    return render_template('index.html', title='Главная')
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html', title='Контакты')
+
+
+@app.route('/about')
+def about_us():
+    return render_template('about.html', title='О нас')
