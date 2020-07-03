ef2bc493cfe28ebbe3604a759a7b72e5ec08b1a1
Architect <greatraksin@icloud.com>
2020-07-03 16:51:59 +0300

    Adding registration

--- mysite/routes.py
+++ mysite/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
 
-from mysite import app
-from mysite.forms import LoginForm
+from mysite import app, db
+from mysite.forms import LoginForm, RegistrationForm
 from mysite.models import User
 
 
@@ -46,3 +46,21 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Вы успешно зарегистрировались!', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('register.html', title='Регистрация', form=form)
