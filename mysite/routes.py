6f02b199038ca46496266359e9c2719d5d75e56e
Architect <greatraksin@icloud.com>
2020-07-03 16:26:27 +0300

    Finish user login/logout functions. Tests is ok

--- mysite/routes.py
+++ mysite/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
+
 from mysite import app
 from mysite.forms import LoginForm
+from mysite.models import User
 
 
 @app.route('/')
@@ -22,7 +25,24 @@ def about_us():
 @app.route('/sign_in', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     if form.validate_on_submit():
-        flash(f'Пользователь {form.username.data}, поле "запомнить": {form.remember_me.data}', 'info')
-        return redirect('/index')
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Неправильное имя пользователя и/или пароль', 'danger')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+
     return render_template('login.html', title='Вход', form=form)
+
+
+@app.route('/sign_out')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
