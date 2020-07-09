94067f5659984195b4a99921e761ad8d951cefb3
Architect <greatraksin@icloud.com>
2020-07-09 21:08:38 +0300

    Improving config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+
+import psycopg2
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -6,3 +9,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'спорим_что_не_угадаешь'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI, sslmode='require')
